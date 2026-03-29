bda318aee74af14c3ea786fcec4d51a82089e529
Mariojuniorr <marioluciosantosjunior@outlook.com>
2026-03-29 13:07:46 -0300

    build: configure tailwindcss, postcss and initialize utility classes

---
+++ Frontend/tailwind.config.ts
@@ -0,0 +1,29 @@
+import type { Config } from "tailwindcss";
+
+const config: Config = {
+  content: [
+    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
+  darkMode: "class",
+  theme: {
+    extend: {
+      backgroundImage: {
+        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
+        "gradient-conic":
+          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      },
+      colors: {
+        nexus: {
+          purple: "#3c1d7c",
+          blue: "#347cd3",
+          pink: "#b43cb3",
+          yellow: "#a89600"
+        }
+      }
+    },
+  },
+  plugins: [],
+};
+export default config;
